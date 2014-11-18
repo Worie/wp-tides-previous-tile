2a78b26146a32c589500adcff4da8345a45375b9
Worie <Worie@MacBook-Pro-Wojciech.local>
2014-11-19 00:43:51 +0100

    Navigating back
    Added support for navigating back with back button

--- backspace.js
+++ backspace.js
@@ -1,3 +1,40 @@
+
+// Change the native hashchange of custom.js
+
+jQuery(window).unbind('hashchange');
+
+
+jQuery(window).on('hashchange',function(ev){
+		var url = location.hash;
+		var minus = 64;
+		if( jQuery('body').hasClass('admin-bar') )
+			var minus = 96;
+			
+		if( jQuery('body').hasClass('resize-off') )
+			var minus = 108;
+			
+		if( jQuery('body').hasClass('resize-off') && jQuery('body').hasClass('admin-bar') )
+			var minus = 140;
+			
+		if( '#home' == url )
+			var minus = 200;
+			
+		if( url )
+			if(typeof jQuery(url).offset() !=='undefined'){
+			jQuery("html, body").animate({ scrollTop: jQuery(url).offset().top - minus }, 500);
+		}else{
+			if(url.indexOf('backspacejs'+parseInt(backspaceCarrige-1))>-1){
+				previous(ev);
+			}
+		}
+
+
+
+});
+
+
+
+
 function previous(ev){
 	backspaceCarrige--;
 	if(backspaceCarrige<=0){history.back();return true;}
@@ -40,33 +77,11 @@ backspacePrevious = jQuery('.button[href^="*"]')[0];
 });
 
 
-jQuery(document).keypress(function(e) {
-
-    if(e.which == 8) {
-		
-		  var d = e.srcElement || e.target;
-        if ((d.tagName.toUpperCase() === 'INPUT' && 
-             (
-                 d.type.toUpperCase() === 'TEXT' ||
-                 d.type.toUpperCase() === 'PASSWORD' || 
-                 d.type.toUpperCase() === 'FILE' || 
-                 d.type.toUpperCase() === 'EMAIL' || 
-                 d.type.toUpperCase() === 'SEARCH' || 
-                 d.type.toUpperCase() === 'DATE' )
-             ) || 
-             d.tagName.toUpperCase() === 'TEXTAREA') {
-            doPrevent = d.readOnly || d.disabled;
-        }
-        else {
-            doPrevent = true;
-        }
-
-		if(doPrevent){previous(e);}
-    }
-});
-
 jQuery('.button[href^="*"],.button[href^="."],.isotope-alt-image,#selectnav> li >a[href^="#"]').click(function(){
 	if(backspaceStop){backspaceStop=false;return false;}
+			
+
+
 	var el = jQuery(this);
 	if(backspaceCarrige<=0){backspaceCarrige=1;}
 	
@@ -83,6 +98,8 @@ jQuery('.button[href^="*"],.button[href^="."],.isotope-alt-image,#selectnav> li 
 	else if(el.hasClass('ajax-link') && el.hasClass('isotope-alt-image')){
 	
 	backspaceHistory[backspaceCarrige]=backspacePortfolioClose;
+				location.href="#backspacejs"+backspaceCarrige;
+
 	backspaceCarrige++;
 	backspaceHistory[backspaceCarrige]=el;
 	
@@ -92,8 +109,11 @@ jQuery('.button[href^="*"],.button[href^="."],.isotope-alt-image,#selectnav> li 
 		
 		}
 	
+
+
 	backspaceCarrige++;
-	
+location.href="#backspacejs"+backspaceCarrige;
+
 	console.log('carrige'+backspaceCarrige);
 	console.log('history'+backspaceHistory);
 });
